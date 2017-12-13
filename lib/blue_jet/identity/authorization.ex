a150968432c8baf70a01be0c6f6f1f59279a6aea
Roy Bao <roy@roybao.com>
2017-12-12 23:47:00 -0800

    Update authorization for test account

--- lib/blue_jet/identity/authorization.ex
+++ lib/blue_jet/identity/authorization.ex
@@ -4,6 +4,86 @@ defmodule BlueJet.Identity.Authorization do
   alias BlueJet.Identity.Account
   alias BlueJet.Repo
 
+  @testable_endpoints [
+    "identity.get_account",
+    "identity.get_user",
+    "identity.get_refresh_token",
+
+    "file_storage.list_external_file",
+    "file_storage.create_external_file",
+    "file_storage.get_external_file",
+    "file_storage.update_external_file",
+    "file_storage.delete_external_file",
+    "file_storage.list_external_file_collection",
+    "file_storage.create_external_file_collection",
+    "file_storage.get_external_file_collection",
+    "file_storage.update_external_file_collection",
+    "file_storage.delete_external_file_collection",
+
+    "inventory.list_stockable",
+    "inventory.create_stockable",
+    "inventory.get_stockable",
+    "inventory.update_stockable",
+    "inventory.delete_stockable",
+    "inventory.list_unlockable",
+    "inventory.create_unlockable",
+    "inventory.get_unlockable",
+    "inventory.update_unlockable",
+    "inventory.delete_unlockable",
+    "inventory.list_depositable",
+    "inventory.create_depositable",
+    "inventory.get_depositable",
+    "inventory.update_depositable",
+    "inventory.delete_depositable",
+
+    "catalogue.list_product",
+    "catalogue.create_product",
+    "catalogue.get_product",
+    "catalogue.update_product",
+    "catalogue.delete_product",
+    "catalogue.list_price",
+    "catalogue.create_price",
+    "catalogue.get_price",
+    "catalogue.update_price",
+    "catalogue.delete_price",
+    "catalogue.list_product_collection",
+    "catalogue.create_product_collection",
+    "catalogue.get_product_collection",
+    "catalogue.update_product_collection",
+    "catalogue.create_product_collection_membership",
+    "catalogue.delete_product_collection_membership",
+
+    "crm.list_customer",
+    "crm.create_customer",
+    "crm.get_customer",
+    "crm.update_customer",
+    "crm.delete_customer",
+    "crm.create_point_transaction",
+
+    "storefront.list_order",
+    "storefront.create_order",
+    "storefront.get_order",
+    "storefront.update_order",
+    "storefront.delete_order",
+    "storefront.list_order_line_item",
+    "storefront.create_order_line_item",
+    "storefront.update_order_line_item",
+    "storefront.delete_order_line_item",
+    "storefront.list_unlock",
+    "storefront.get_unlock",
+
+    "billing.list_payment",
+    "billing.create_payment",
+    "billing.get_payment",
+    "billing.update_payment",
+    "billing.list_card",
+    "billing.update_card",
+    "billing.delete_card",
+    "billing.create_refund",
+
+    "data_trading.create_data_import"
+  ]
+
   @role_endpoints %{
     "anonymous" => [
       "identity.create_user"
@@ -360,15 +440,11 @@ defmodule BlueJet.Identity.Authorization do
   def authorize(vas, endpoint) when map_size(vas) == 0 do
     authorize("anonymous", endpoint)
   end
-  def authorize(%{ user_id: user_id, account_id: account_id }, endpoint) do
+  def authorize(vas = %{ account_id: account_id }, endpoint) do
     account = Repo.get!(Account, account_id)
-
-    account_id = if account.mode == "live" do
-      account.id
-    else
-      account.live_account_id
-    end
-
+    authorize(vas, account, endpoint)
+  end
+  def authorize(%{ user_id: user_id }, %{ id: account_id, mode: "live" }, endpoint) do
     with %AccountMembership{ role: role } <- Repo.get_by(AccountMembership, account_id: account_id, user_id: user_id),
          {:ok, role} <- authorize(role, endpoint)
     do
@@ -378,7 +454,14 @@ defmodule BlueJet.Identity.Authorization do
       {:error, role} -> {:error, :role_not_allowed}
     end
   end
-  def authorize(%{ account_id: account_id }, endpoint) do
+  def authorize(vas, %{ mode: "test", live_account_id: account_id }, endpoint) do
+    # Check if the endpoint is testable
+    case Enum.find(@testable_endpoints, fn(testable_endpoint) -> endpoint == testable_endpoint end) do
+      nil -> {:error, :test_account_not_allowed}
+      other -> authorize(vas, %{ id: account_id, mode: "live" }, endpoint)
+    end
+  end
+  def authorize(%{ account_id: account_id }, %{ mode: "live" }, endpoint) do
     authorize("guest", endpoint)
   end
   def authorize(role, target_endpoint) when is_bitstring(role) do
