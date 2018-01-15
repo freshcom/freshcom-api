488aef8939866388d9174dfc2d14e14f9219bdc0
Roy Bao <roy@roybao.com>
2018-01-14 20:38:26 -0800

    Add postgres username for test

--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,7 @@ config :blue_jet, BlueJet.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "blue_jet_test",
   hostname: if(System.get_env("CI"), do: "postgres", else: "localhost"),
+  username: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :comeonin, :bcrypt_log_rounds, 4
