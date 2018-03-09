b65c6bf611385d07c1d00098d63b6b9894afd39f
Roy Bao <roy@roybao.com>
2018-03-09 13:58:00 -0800

    Chore(mix): use elixir 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BlueJet.Mixfile do
     [
       app: :blue_jet,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
