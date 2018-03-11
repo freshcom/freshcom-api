cb398b9220591b9b47cfd6e1f20bd55330f6752b
Roy Bao <roy@roybao.com>
2018-03-10 18:29:58 -0800

    Chore(goods): remove unused alias

--- lib/blue_jet/goods/event_handler.ex
+++ lib/blue_jet/goods/event_handler.ex
@@ -1,7 +1,6 @@
 defmodule BlueJet.Goods.EventHandler do
   @behaviour BlueJet.EventHandler
 
-  alias BlueJet.Repo
   alias BlueJet.Goods.Service
 
   def handle_event("identity.account.reset.success", %{ account: account = %{ mode: "test" } }) do
