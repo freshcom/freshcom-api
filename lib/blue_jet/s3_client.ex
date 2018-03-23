970ecc245042cf27ed868b2d065fab6afd260855
Roy Bao <roy@roybao.com>
2018-03-22 22:06:54 -0700

    Fix(file_storage): only use CDN if a host is provided

--- lib/blue_jet/s3_client.ex
+++ lib/blue_jet/s3_client.ex
@@ -1,12 +1,24 @@
 defmodule BlueJet.S3.Client do
-  def get_presigned_url(key, method) do
-    config = if method == :get do
-      ExAws.Config.new(:s3)
-      |> Map.put(:host, System.get_env("CDN_HOST"))
-    else
-      ExAws.Config.new(:s3)
-    end
+  def get_presigned_url(key, :get) do
+    config = ExAws.Config.new(:s3)
 
+    if System.get_env("CDN_HOST") && String.length(System.get_env("CDN_HOST")) > 0 do
+      expires_in = 3600
+      host = System.get_env("CDN_HOST")
+      url = "https://#{host}/#{key}"
+      datetime = :calendar.universal_time
+      {:ok, url} = ExAws.Auth.presigned_url(:get, url, :s3, datetime, config, expires_in, [])
+
+      url
+    else
+      {:ok, url} = ExAws.S3.presigned_url(config, :get, System.get_env("AWS_S3_BUCKET_NAME"), key)
+
+      url
+    end
+  end
+
+  def get_presigned_url(key, method) do
+    config = ExAws.Config.new(:s3)
     {:ok, url} = ExAws.S3.presigned_url(config, method, System.get_env("AWS_S3_BUCKET_NAME"), key)
 
     url
