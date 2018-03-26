65d56514efe8d88ae18a23ca1890d3017b73c24d
Roy Bao <roy@roybao.com>
2018-03-26 14:56:57 -0700

    Fix(ecto): increase timeout to 30 seconds

--- config/prod.exs
+++ config/prod.exs
@@ -25,6 +25,7 @@ config :blue_jet, BlueJet.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  timeout: 30_000,
   ssl: true
 
 # ## SSL Support
