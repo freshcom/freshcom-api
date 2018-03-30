63d26a6885b0ea2d5a6e32bc5535429d032b9e07
Roy Bao <roy@roybao.com>
2018-03-30 16:41:53 -0700

    Feature(fulfillment): unlock should include customer

--- lib/blue_jet_web/views/unlock_view.ex
+++ lib/blue_jet_web/views/unlock_view.ex
@@ -5,12 +5,17 @@ defmodule BlueJetWeb.UnlockView do
   attributes [:inserted_at, :updated_at]
 
   has_one :unlockable, serializer: BlueJetWeb.UnlockableView, identifiers: :always
+  has_one :customer, serializer: BlueJetWeb.CustomerView, identifiers: :always
 
   def type do
     "Unlock"
   end
 
   def unlockable(%{ unlockable_id: nil }, _), do: nil
-  def unlockable(%{ unlockable_id: unlockable_id, unlockable: nil }, _), do: %{ id: unlockable_id, type: "File" }
+  def unlockable(%{ unlockable_id: unlockable_id, unlockable: nil }, _), do: %{ id: unlockable_id, type: "Unlockable" }
   def unlockable(%{ unlockable: unlockable }, _), do: unlockable
+
+  def customer(%{ customer_id: nil }, _), do: nil
+  def customer(%{ customer_id: customer_id, customer: nil }, _), do: %{ id: customer_id, type: "Customer" }
+  def customer(%{ customer: customer }, _), do: customer
 end
