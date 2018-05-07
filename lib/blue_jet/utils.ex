1b93f6dabdb497eb66c3b366ce4c5e2aa2f3df02
Roy Bao <roy@roybao.com>
2018-05-06 22:19:48 -0700

    Refactor(utils): remove unused function

--- lib/blue_jet/utils.ex
+++ lib/blue_jet/utils.ex
@@ -1,12 +1,4 @@
 defmodule BlueJet.Utils do
-  def intersect_list(list1, list2) do
-    list1 -- (list1 -- list2)
-  end
-
-  def sum(structs, field) do
-    Enum.reduce(Enum.map(structs, fn(struct) -> struct[field] end), 0, &+/2)
-  end
-
   def parameterize(s) do
     s
     |> String.downcase()
