37bc2ace2c48455a85f64de00d5e20b97a042bd2
Roy Bao <roy@roybao.com>
2018-05-16 21:20:53 -0700

    Refactor: remove unused functions and use :code for extracting errors

--- lib/blue_jet_web/controllers/helpers.ex
+++ lib/blue_jet_web/controllers/helpers.ex
@@ -1,57 +1,19 @@
 defmodule BlueJetWeb.Controller.Helpers do
-  import Ecto.Query
 
-  def paginate(query, size: size, number: number) do
-    limit = size
-    offset = size * (number - 1)
-
-    query
-    |> limit(^limit)
-    |> offset(^offset)
-  end
-
-  def search(model, _, nil, _), do: model
-  def search(model, _, "", _), do: model
-  def search(model, columns, keyword, "en") do
-    keyword = "%#{keyword}%"
-
-    Enum.reduce(columns, model, fn(column, query) ->
-      from q in query, or_where: ilike(fragment("?::varchar", field(q, ^column)), ^keyword)
-    end)
-  end
-  def search(model, columns, keyword, locale) do
-    keyword = "%#{keyword}%"
-
-    Enum.reduce(columns, model, fn(column, query) ->
-      if Enum.member?(model.translatable_fields(), column) do
-        column = Atom.to_string(column)
-        from q in query, or_where: ilike(fragment("?->?->>?", q.translations, ^locale, ^column), ^keyword)
-      else
-        from q in query, or_where: ilike(fragment("?::varchar", field(q, ^column)), ^keyword)
-      end
-    end)
-  end
-
-  def filter_by(query, filter) do
-    filter = Enum.filter(filter, fn({_, value}) -> value end)
-    from q in query, where: ^filter
-  end
-
-  def extract_errors(%Ecto.Changeset{ valid?: false, errors: errors }) do
+  def extract_errors(%{ valid?: false, errors: errors }) do
     extract_errors(errors)
   end
-  def extract_errors(changeset = %Ecto.Changeset{ valid?: true }), do: changeset
+  def extract_errors(changeset = %{ valid?: true }), do: changeset
   def extract_errors(errors) do
     Enum.reduce(errors, [], fn({ field, { msg, opts } }, acc) ->
       msg = Enum.reduce(opts, msg, fn({ key, value }, acc) ->
         String.replace(acc, "%{#{key}}", to_string(value))
       end)
 
-      {full_error_message, opts} = Keyword.pop(opts, :full_error_message)
-      title = if full_error_message do
-        msg
-      else
+      title = if opts[:validation] do
         "#{humanize(field)} #{msg}"
+      else
+        msg
       end
 
       mc = cond do
@@ -104,17 +66,6 @@ defmodule BlueJetWeb.Controller.Helpers do
       Map.put(acc, Inflex.camelize(key, :lower), value)
     end)
   end
-
-  # def translate_collection(collection, "en"), do: collection
-  # def translate_collection(collection, locale) do
-  #   Enum.map(collection, fn(item) -> translate(item, locale) end)
-  # end
-
-  # def translate(collection, "en"), do: collection
-  # def translate(struct, locale) do
-  #   t_attributes = Map.new(Map.get(struct.translations, locale, %{}), fn({k, v}) -> { String.to_atom(k), v } end)
-  #   Map.merge(struct, t_attributes)
-  # end
 
   def underscore_value(map, keys) do
     Enum.reduce(map, map, fn({k, v}, acc) ->
